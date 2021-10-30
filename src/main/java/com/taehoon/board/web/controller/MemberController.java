3f681d5a6b3220a1c0f8889a5db6f4191ee1e4cb
iamtaehoon <kimth9981@naver.com>
2021-10-30 17:05:01 +0900

    중복 검사를 만들고 싶은데, 자바스크립트 사용 없이 만들수 없을 것 같음. / 지금 만들고 있는 방법이 아닌 거같음.

--- src/main/java/com/taehoon/board/web/controller/MemberController.java
+++ src/main/java/com/taehoon/board/web/controller/MemberController.java
@@ -7,12 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,17 +27,25 @@ public class MemberController {
 
     @PostMapping("/join") //메인페이지로 보내야지
     public String join(@ModelAttribute("member") @Valid MemberCreateDto memberCreateDto, BindingResult result) {
-        System.out.println("memberCreateDto = " + memberCreateDto.getEmail());
-        System.out.println("memberCreateDto = " + memberCreateDto.getPassword());
-        System.out.println("memberCreateDto = " + memberCreateDto.getPhoneNum());
-        System.out.println("memberCreateDto = " + memberCreateDto.getUserId());
-        System.out.println("memberCreateDto = " + memberCreateDto.getBirth());
-        System.out.println("memberCreateDto = " + memberCreateDto.getGender());
-
+        System.out.println("memberCreateDto = " + memberCreateDto);
         if (result.hasErrors()) {
             return "member/joinForm";
         }
 
         return "redirect:/join";
+    }
+
+    @GetMapping("check-id-duplicate")
+    public String check_id_duplicate(@RequestParam String userId, BindingResult bindingResult) {
+
+        List<Member> memberByUserId = memberService.findMemberByUserId(userId);
+        if (memberByUserId.isEmpty()) {
+            //올바른로직
+            return "redirect:/join";
+        }
+        //틀린 로직
+        bindingResult.reject("","아이디가 중복되었습니다.");
+        return "redirect/join";
+
     }
 }
--- src/main/java/com/taehoon/board/web/dto/member/MemberCreateDto.java
+++ src/main/java/com/taehoon/board/web/dto/member/MemberCreateDto.java
@@ -3,6 +3,7 @@ package com.taehoon.board.web.dto.member;
 import com.taehoon.board.domain.Gender;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.EnumType;
@@ -12,10 +13,11 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter @Setter
 // DTO는 세터를 써도 될까? -> 이거 안쓰면 검증이 제대로 되나? 일단 setter로 만들고 그다음에 다른 방식 써보자.
-// 정규 표현식?? 이거 공부해서 입력양식 내가 만들어보자.
+// 정규 표현식?? 이거 공부해서 입력양식 내가 만들어보자. -> 사실 필요한건 검색하면 다 나오는데, 읽을 수 있을 정도로만 공부하자. 만들수 있을 정도 x
 public class MemberCreateDto {
 
     @NotBlank(message = "아이디를 입력해주세요")
@@ -25,19 +27,24 @@ public class MemberCreateDto {
     @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}", message = "비밀번호는 숫자, 영어 소문자, 대문자, 특수문자로 이루어진 8-20자의 문자어야 합니다.")
     private String password;
 
-    @NotBlank(message = "이메일을 입력해주세요")
+//    @NotBlank(message = "이메일을 입력해주세요")
     @Email(message = "이메일 양식이 잘못되었습니다.")
     private String email;
 
     @NotBlank(message = "전화번호를 입력해주세요")
-    @Pattern(regexp = "[0-9]{10,11}",message = "전화번호 양식이 잘못되었습니다.")
+    @Pattern(regexp = "(010[0-9]{8})|(011[0-9]{7})",message = "전화번호 양식이 잘못되었습니다.")
+    //0~9까지 하나 선택 , 10개 혹은 11
     private String phoneNum;
 
     @NotBlank(message = "성별을 선택해 주세요.")
     private String gender;
 
+    //    @Past(message = "미래는 생년월일이 될 수 없습니다.")
+    //애는 왜 안되지?
     @NotBlank(message = "생년월일을 선택해주세요")
-//    @Past(message = "미래는 생년월일이 될 수 없습니다.")
+    // 음... 아무리 찾아봐도 form 내부 값을 객체로 바꾸는걸 못찾겠다.
+    // String으로 받아와서, 양식이 맞나, 미래 값이 아니게 만드는건 진짜 엔티티에서 해줘야할듯.
+    @Pattern(regexp = "(19|20)[0-9]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])",message = "정확한 날짜를 입력해주세요.")
     private String birth; //생년월일
 
 }
